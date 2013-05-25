942a880a72b6faeb4dddafe7361df37c89396f71
andreas <andi@kiste.local>
2013-05-25 23:19:06 +0200

    u

--- bergradler.geo.model/src/bergradler/geo/model/primitives/Node.scala
+++ bergradler.geo.model/src/bergradler/geo/model/primitives/Node.scala
@@ -2,8 +2,6 @@ package bergradler.geo.model.primitives
 
 import scala.collection.mutable.Map
 
-class Node(lat: Double, lon: Double, _ele: Option[Double] = None) extends Primitive {
-
-  val ele = _ele
+class Node(lat: Double, lon: Double, val ele: Option[Double] = None) extends Primitive {
 
 }
